1d87827228f70e68d0c390cffd167d19ee8c83f0
eugenetaan <80191549+eugenetaan@users.noreply.github.com>
2021-12-14 16:01:29 +0800

    Update Users_API.py

--- Users/Users_API.py
+++ Users/Users_API.py
@@ -3,15 +3,15 @@ from flask import Flask, render_template, flash, redirect, url_for, request, jso
 from flask import Blueprint
 import sys
 
-sys.path.append("../db")
+sys.path.append("../")
 
 users_api = Blueprint("users", __name__)
 
-users_api.route("/")
+@users_api.route("/")
 def hello():
     return "Hello here are the users"
 
-users_api.route("/users", methods=["GET"])
+@users_api.route("/users", methods=["GET"])
 
 def get_users():
         try:
@@ -21,25 +21,31 @@ def get_users():
 
         if not userID:
             try:
-                data = list(db.users.find({"_id": 0}).sort("block", 1))
+                data = list(db.Users.find({}, {"_id" : 0 }))
                 response = {"status": "success", "data": data}
             except Exception as e:
                 print(e)
                 return {"err": str(e), "status": "failed"}, 400
             
         else:
+            print(2)
             try:
-                data = list(db.users.find_one({"userID": userID}))
+                data = db.Users.find_one({"userID": userID}, {"_id" : 0 })
+                print(data)
             except Exception as e:
+                print(e)
                 return {"err": str(e), "status": "failed"}, 400
 
-            if len(data) == 0:
-                raise Exception("User not found")
+            
+            if data == None:
+                return {"err": "No such User", "status": "failed"}, 400
 
             response = {"status": "success", "data": data}
         
         return make_response(response)
     
+
+
     
 
 
